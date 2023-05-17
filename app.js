127540e0eccb7bef234ee9f9f203abf184e635f8
Oleg Smitkevich <o.smitkevich@gmail.com>
2023-05-17 19:33:17 +0300

    smallfix3

--- app.js
+++ app.js
@@ -26,11 +26,12 @@ app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string().dataUri().min(2).max(30),
+    avatar: Joi.string().min(2).max(30),
     email: Joi.string().required().email(),
     password: Joi.string().required().pattern(/^[a-zA-Z0-9]{3,30}$/),
   }),
 }), createUser);
+
 app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().email(),
