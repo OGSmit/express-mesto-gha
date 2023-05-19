d59560d57421684caf291f776ed58208909a49f0
Oleg Smitkevich <o.smitkevich@gmail.com>
2023-05-19 18:41:05 +0300

    hotfixReview

--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -1,8 +1,11 @@
 const errorHandler = (err, req, res, next) => {
+  if (err === 11000) {
+    return res.status(409).send({ message: 'пользователь с таким email - существует' });
+  }
   const statusCode = err.statusCode || 500;
   const message = statusCode === 500 ? 'На сервере произошла ошибка' : err.message;
   res.status(statusCode).send({ message });
-  next();
+  return next();
 };
 
 module.exports = errorHandler;
