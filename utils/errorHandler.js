e35e20543a631ab3a3d40bdbe3a9ab6112eeb60a
Oleg Smitkevich <o.smitkevich@gmail.com>
2023-05-19 18:26:09 +0300

    fixReview

--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -1,23 +1,3 @@
-// const express = require('express');
-
-// function errorHandler(err, req, res, next) {
-//   console.log(err);
-//   if (err === 11000) {
-//     return res.status(409).send({ message: 'пользователь с таким email - существует' });
-//   }
-//   const statusCode = 500 || err.message;
-//   res
-//     .status(statusCode)
-//     .send({
-//       message: statusCode === 500
-//         ? 'На сервере произошла ошибка'
-//         : err.message,
-//     });
-//   return next();
-// }
-
-// module.exports = errorHandler;
-
 const errorHandler = (err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = statusCode === 500 ? 'На сервере произошла ошибка' : err.message;
